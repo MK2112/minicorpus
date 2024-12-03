c5242705eff96f28772e47fc3d03c3a0934a48ba
Marcus <29037335+MK2112@users.noreply.github.com>
2024-12-03 17:31:06 +0100

    first working distillation pipe

--- 03_distill_pile_embed.py
+++ 03_distill_pile_embed.py
@@ -44,6 +44,7 @@ class MiniPileDistiller:
             raise ValueError(f"Must exclude exactly {self.config.num_clusters_to_exclude} clusters to adhere to the paper. You provided {len(self.config.excluded_clusters)} clusters.")
         self._load_total_cluster_info()
         self._compute_shard_scopes()
+        self.shard_counter: int = 0
     
     def _load_total_cluster_info(self):
         # Load general cluster information JSON file, populate class attribute
@@ -108,109 +109,95 @@ class MiniPileDistiller:
         all_indices = list(range(total_docs))
         # I want at all cost to avoid having consecutive indices from the same cluster overly represented in the same split
         self.config.rng.shuffle(all_indices)
-        return {'train': all_indices[:self.config.train_count],
+        split = {'train': all_indices[:self.config.train_count],
                 'validation': all_indices[self.config.train_count:(self.config.train_count + self.config.val_count)],
                 'test': all_indices[(self.config.train_count + self.config.val_count):(self.config.train_count + self.config.val_count + self.config.test_count)]}
+        return split
     
+    def _process_cluster(self, cluster: int) -> Set[int]:
+        if cluster not in self.config.excluded_clusters:
+            valid_indices = self._get_idxs_for_cluster(cluster)
+            return set(self._sample_cluster_docs(valid_indices))
+        return set()
+
     def build_minipile(self):
-        # Prepare list of document indices to extract
-        total_doc_indices_to_extract = set()
-        for cluster in tqdm(range(self.config.num_clusters), desc="Assembling Indices to Extract", unit="cluster"):
-            if cluster not in self.config.excluded_clusters:
-                total_doc_indices_to_extract.update(self._sample_cluster_docs(self._get_idxs_for_cluster(cluster)))
-        
+        # Prepare list of document indices to extract using parallel processing
+        with Pool(processes=cpu_count() // 2) as pool:
+            results = pool.map(self._process_cluster, range(self.config.num_clusters))
+
+        # Combine results from all workers
+        total_doc_indices_to_extract = set().union(*results)
+
         # Generate data splits
         print(f"[+] Total documents to extract: {len(total_doc_indices_to_extract)}. Shuffling and splitting.")
         data_splits = self._shuffle_split(len(total_doc_indices_to_extract))
         
-        # Process and write documents in splits, in parallel, in the sun, with a drink
+        # Process and write documents in splits
         for split_name, split_indices in tqdm(data_splits.items(), desc="Processing Splits", unit="split"):
-            self._process_split(total_doc_indices_to_extract, split_indices, split_name)
+            self._process_split(split_indices, split_name)
         
         print(f"[+] MiniPile created with {len(total_doc_indices_to_extract)} documents across train/val/test splits.")
     
-    def _process_split(self, all_indices: Set[int], split_indices: List[int], split_name: str):
+    def _process_split(self, split_indices: List[int], split_name: str):
+        # Convert split_indices to a set for faster lookup
+        split_indices_set = set(split_indices)
+        
         # Group indices by shard
         indices_by_shard = {}
         for idx in tqdm(split_indices, desc=f"Split {split_name} Shard Grouping", unit="doc"):
-            if idx in all_indices:
-                shard_idx = self._shard_with_idx(idx)
-                indices_by_shard.setdefault(shard_idx, []).append(idx)
+            shard_idx = self._shard_with_idx(idx)
+            if shard_idx not in indices_by_shard:
+                indices_by_shard[shard_idx] = []
+            if idx in split_indices_set:
+                indices_by_shard[shard_idx].append(idx)
 
-        parquet_files = sorted(Path(self.config.embd_dir).glob("shard_*.parquet")) # 9-digit zero-padded shard indices in filename
+        assert sum(len(indices) for indices in indices_by_shard.values()) == len(split_indices)
+        parquet_files = sorted(Path(self.config.embd_dir).glob("shard_*.parquet"))
 
-        with Manager() as manager:
-            shard_counter = manager.Value('i', 0)  # Shared counter
-            counter_lock = manager.Lock()  # Lock to ensure safe increments
-            tasks = [(shard_idx, indices, parquet_files[shard_idx], split_name, self.config, self.shard_idxs, shard_counter, counter_lock) for shard_idx, indices in indices_by_shard.items()]
-            # Process shards #Parallelly
-            with Pool(cpu_count() // 2) as pool:
-                pool.starmap(self._process_shard, tasks)
+        for shard_idx, indices in tqdm(indices_by_shard.items(), desc=f"Processing {split_name} Shards", unit="shard"):
+            file_path = parquet_files[shard_idx]
+            self._process_shard(shard_idx, indices, file_path, split_name)
 
-    @staticmethod
-    def _process_shard(shard_idx: int, indices_to_extract: List[int], file_path: Path, split_name: str, config: DistillConfig,
-                       shard_idxs: List[int], shard_counter, counter_lock):
-        # Convert indices_to_extract to local shard indices
-        local_indices = [idx - shard_idxs[shard_idx] for idx in indices_to_extract]
+    def _process_shard(self, shard_idx: int, indices_to_extract: List[int], file_path: Path, split_name: str) -> int:
+        # Convert global indices to local shard indices
+        local_indices = [idx - self.shard_idxs[shard_idx] for idx in indices_to_extract]
         
-        # Create a pyarrow dataset and apply filtering
-        parquet_ds = ds.dataset(file_path)
-        filter_expr = pa.compute.is_in(
-            pa.array(local_indices), parquet_ds.schema.field("idx")
-        )
-        table = parquet_ds.to_table(filter=filter_expr, columns=["idx", "text"])
-        
-        # Process filtered rows directly
-        current_shard_docs = [{"idx": row["idx"].as_py(), "text": row["text"].as_py()} for row in table]
+        # Read the entire Parquet file
+        table = pq.read_table(file_path)
+        df = table.to_pandas()
+        selected_df = df.iloc[local_indices].copy() # Select rows matching the local indices using boolean indexing mask
+        del df, table
+        # Add back the global indices
+        selected_df.loc[:, 'idx'] = list(indices_to_extract)
+        current_shard_docs = selected_df.to_dict('records')
         
         # Write out shards based on the configured shard size
-        num_docs = len(current_shard_docs)
-        with tqdm(total=num_docs, desc=f"Processing Shard {shard_idx}", unit="doc") as pbar:
-            # Write out shards based on the configured shard size
-            while len(current_shard_docs) >= config.output_shard_size:
-                with counter_lock:
-                    shard_id = shard_counter.value
-                    shard_counter.value += 1
-                
-                MiniPileDistiller._write_parquet_shard(current_shard_docs[:config.output_shard_size], shard_id, split_name, config)
-                del current_shard_docs[:config.output_shard_size]
+        with tqdm(total=len(current_shard_docs), desc=f"Processing Shard {shard_idx}", unit="doc") as pbar:
+            while len(current_shard_docs) >= self.config.output_shard_size:
+                self._write_parquet_shard(current_shard_docs[:self.config.output_shard_size], self.shard_counter, split_name)
+                del current_shard_docs[:self.config.output_shard_size]
+                self.shard_counter += 1
                 gc.collect()
-                pbar.update(config.output_shard_size)
+                pbar.update(self.config.output_shard_size)
 
             # Handle remaining documents
             if current_shard_docs:
-                with counter_lock:
-                    shard_id = shard_counter.value
-                    shard_counter.value += 1
-                
-                MiniPileDistiller._write_parquet_shard(current_shard_docs, shard_id, split_name, config)
-                del current_shard_docs[:]
-                gc.collect()
+                self._write_parquet_shard(current_shard_docs, self.shard_counter, split_name)
+                self.shard_counter += 1
                 pbar.update(len(current_shard_docs))
+        del selected_df
+        gc.collect()
 
-        # Handle remaining documents
-        if current_shard_docs:
-            with counter_lock:
-                shard_id = shard_counter.value
-                shard_counter.value += 1
-            
-            MiniPileDistiller._write_parquet_shard(current_shard_docs, shard_id, split_name, config)
-            del current_shard_docs
-            gc.collect()
-
-    @staticmethod
-    def _write_parquet_shard(docs: List[Dict], shard_index: int, split_name: str, config: DistillConfig):
+    def _write_parquet_shard(self, docs: List[Dict], shard_index: int, split_name: str):
         # Write a shard of documents to a Parquet file
         df = pa.table({'text': [doc['text'] for doc in docs], 'pile_idx': [doc['idx'] for doc in docs]})
-        output_path = config.output_dir / f"minipile_{config.edition}_{split_name}_shard_{shard_index:09d}.parquet"
+        output_path = self.config.output_dir / f"minipile_{self.config.edition}_{split_name}_shard_{shard_index:09d}.parquet"
         pq.write_table(df, output_path)
         del df
 
 if __name__ == "__main__":
     config = DistillConfig()
-    print("[!] Loaded MiniPile Creation Config")
     distiller = MiniPileDistiller(config)
-    print("[!] Initialized Distiller")
     distiller.build_minipile()
 
 # tmux new -s minipile
