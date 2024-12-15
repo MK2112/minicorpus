d23ea070393663955be9cf58e32e8fe4c9ded3fe
Marcus <29037335+MK2112@users.noreply.github.com>
2024-12-15 01:36:33 +0100

    updated k440 training for fixed version

--- 03_train_160M_k440.py
+++ 03_train_160M_k440.py
@@ -71,16 +71,16 @@ def training():
 
     minipile_train = load_dataset("parquet",
                                   data_files={
-                                      "train": str(base_path / "MiniPile_k440" / "minipile_k440_train_shard_*.parquet"),
+                                      "train": str(base_path / "MiniPile_k440_fixed" / "minipile_k440_fixed_train_shard_*.parquet"),
                                   },
-                                  cache_dir=str(base_path / "MiniPile_k440_Cache"),
+                                  cache_dir=str(base_path / "MiniPile_k440_fixed_Cache"),
                                   split="train")
 
     minipile_val = load_dataset("parquet",
                                 data_files={
-                                    "validation": str(base_path / "MiniPile_k440" / "minipile_k440_validation_shard_*.parquet"),
+                                    "validation": str(base_path / "MiniPile_k440_fixed" / "minipile_k440_fixed_validation_shard_*.parquet"),
                                 },
-                                cache_dir=str(base_path / "MiniPile_k440_Cache"),
+                                cache_dir=str(base_path / "MiniPile_k440_fixed_Cache"),
                                 split="validation")
 
     tokenizer = AutoTokenizer.from_pretrained(base_path / "pythia160m_dedup_untrained", use_fast=True, local_files_only=True)
@@ -98,14 +98,14 @@ def training():
                          max_length=2048,
                          return_special_tokens_mask=True)
 
-    if os.path.exists(base_path / "minipile_k440_train_tokenized"):
-        minipile_train_tokenized = load_dataset("arrow", data_files=str(base_path / "minipile_k440_train_tokenized/*.arrow"), split="train")
-        minipile_val_tokenized = load_dataset("arrow", data_files=str(base_path / "minipile_k440_val_tokenized/*.arrow"), split="train")
+    if os.path.exists(base_path / "minipile_k440_fixed_train_tokenized"):
+        minipile_train_tokenized = load_dataset("arrow", data_files=str(base_path / "minipile_k440_fixed_train_tokenized/*.arrow"), split="train")
+        minipile_val_tokenized = load_dataset("arrow", data_files=str(base_path / "minipile_k440_fixed_val_tokenized/*.arrow"), split="train")
     else:
         minipile_train_tokenized = minipile_train.map(tokenize, batched=True, remove_columns=minipile_train.column_names) # retain only new fields from tokenization
         minipile_val_tokenized = minipile_val.map(tokenize, batched=True, remove_columns=minipile_val.column_names)
-        minipile_train_tokenized.save_to_disk(base_path / "minipile_k440_train_tokenized")
-        minipile_val_tokenized.save_to_disk(base_path / "minipile_k440_val_tokenized")
+        minipile_train_tokenized.save_to_disk(base_path / "minipile_k440_fixed_train_tokenized")
+        minipile_val_tokenized.save_to_disk(base_path / "minipile_k440_fixed_val_tokenized")
 
     batch_size = 8     # 16 is too much for 4xA6000
     total_batch = 1024
@@ -134,8 +134,8 @@ def training():
     else:
         print("No CUDA-capable GPUs available")
 
-    output_dir = str(base_path / "pythia160m_minipile_k440_trained")
-    log_dir = str(base_path / "160m_minipile_k440_logs")
+    output_dir = str(base_path / "pythia160m_minipile_k440_fixed_trained")
+    log_dir = str(base_path / "160m_minipile_k440_fixed_logs")
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(log_dir, exist_ok=True)
 
@@ -190,15 +190,16 @@ def training():
     trainer.train()
 
     # Why is this a two-step process?!
-    trainer.save_model(str(base_path / "pythia160m_minipile_k440_trained")) # This saves the model weights
+    trainer.save_model(str(base_path / "pythia160m_minipile_k440_fixed_trained")) # This saves the model weights
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     training()
 
 # tmux new -s 160m_minipile_k440
 # conda activate minipile
 # torchrun --nproc_per_node=4 03_train_160M_k440.py
-# I ran with CUDA_VISIBLE_DEVICES=1,2,3 torchrun --nproc_per_node=3 03_train_160M_k440.py
+# I ran with CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=1 03_train_160M_k440.py
 # May need to reset in later run, I don't know
 # Detach from tmux session: Ctrl-b followed by d
 # Reattach to tmux session: tmux attach -t 160m_minipile_k440
