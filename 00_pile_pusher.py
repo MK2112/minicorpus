3c533516475dbb7708045f34c0ec2eb6b742ae1c
Marcus <29037335+MK2112@users.noreply.github.com>
2025-01-26 21:30:39 +0100

    added option for xl datasets

--- 00_pile_pusher.py
+++ 00_pile_pusher.py
@@ -13,6 +13,12 @@ def push_dataset(repo_id):
                            'test': load_dataset('parquet', data_files=str(base_path / 'MiniPile_DensityProportionedHigh/minipile_DensityProportionedHigh_test_shard_*.parquet'), split='train')})
     dataset.push_to_hub(repo_id)
 
+def push_xl_dataset(repo_id):
+    # Dataset is bigger than 300GB, ask datasets@huggingface.co to grant more storage
+    # See https://huggingface.co/docs/hub/storage-limits on what to inform about exactly
+    dataset = DatasetDict({'train': load_dataset('parquet', data_files=str(base_path / 'RefinedWeb_Embd/shard_*.parquet'), split='train', streaming=True)})
+    dataset.push_to_hub(repo_id)
+
 def push_jsonl_dataset(repo_id):
     dataset = load_dataset("json", data_files=glob.glob(str(base_path / "MiniPile_BatchKMeans_Double/clustering_results/cluster_results_chunk_*.jsonl")))
     dataset.push_to_hub(repo_id)
@@ -26,5 +32,6 @@ def push_model(repo_id):
 
 if __name__ == "__main__":
     #push_dataset("Marcus2112/minipile_low-density")
-    push_jsonl_dataset(repo_id="Marcus2112/pile_dedup_embeddings_clusters_k440")
+    #push_jsonl_dataset(repo_id="Marcus2112/pile_dedup_embeddings_clusters_k440")
     #push_model(repo_id="Marcus2112/pythia-160m-minipile_low-density")
+    push_xl_dataset(repo_id="Marcus2112/refinedweb-embedded")
